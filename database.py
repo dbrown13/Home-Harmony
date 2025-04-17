008f6b8a8bba481256b6849794133ec67ac581bc
Deryn Brown <109038259+dbrown13@users.noreply.github.com>
2025-04-17 00:11:46 -0500

    Backend  - database, models, app

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import sqlite3
 from sqlite3 import Connection
 from typing import List, Union
 from pydantic import ValidationError
-from models import Project, Projects, UserProject, UserProjectId, UserHashed, UserHashedIndex, Decor
-
+from models import Project, Projects, UserProject, UserProjectId, UserHashed, UserHashedIndex, UserImage, ImageRetrieve, Images
+import os
 
 def create_user(connection: Connection,
                 user: UserHashed)->bool:
@@ -64,6 +64,7 @@ def create_new_project(connection: Connection,
             """,
             project.model_dump()
         )
+        connection.commit()
 
 def get_projects(connection: Connection)->Projects:
     with connection:
@@ -119,21 +120,95 @@ def delete_project_by_id(connection: Connection, proj_id: int)->bool:
         connection.commit()
         #return Projects(projects = [Project.model_validate(dict(project)) for project in cur])
         return True
+    
+def add_image(connection: Connection, image: UserImage)->bool:
 
-def add_decor(connection: Connection, decor: Decor)->bool:
+    # Insert the image into the database
     with connection:
         cur = connection.cursor()
         cur.execute(
             """
-            INSERT INTO decor (decor_name, decor_type, decor_desc, user_id, project_id)
+            INSERT INTO images (image_title, image_desc, image_data, user_id, project_id)
             VALUES
-            ( :decor_name, :decor_type, :decor_desc, :user_id, :project_id)
+            ( :image_title, :image_desc, :image_data, :user_id, :project_id)
             """,
-            decor.model_dump()
+            image.model_dump()
         )
     connection.commit()
     return True
 
+def get_images_by_project_id(connection: Connection, project_id: int)->Images:
+    with connection:
+        cur = connection.cursor()
+        cur.execute(
+            """
+            SELECT image_title, image_desc, image_data, user_id, project_id
+            FROM images
+            WHERE project_id =?
+            """,
+            (project_id,),
+        )
+        return Images(images = [UserImage.model_validate(dict(image)) for image in cur])
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    print("Convert to binary format")
+    with open(filename, 'rb') as file:
+        blobData = file.read()
+    return blobData
+
+#def insertBLOB(empID, name, photo, resumeFile):
+def insertBLOB(connection: Connection, image: UserImage)->bool:
+    with connection:
+        cur = connection.cursor()
+        sqlite_insert_blob_query = """ INSERT 
+            INTO images (image_title, image_desc, image_data, image_type, user_id, project_id) 
+            VALUES (?, ?, ?, ?, ?, ?)
+        """
+        image_data = convertToBinaryData(image.image_filename)
+        # Convert data into typle format
+        data_tuple = (image.image_title, image.image_desc, image_data, image.image_type, image.user_id, image.project_id)
+        # Insert the BLOB data into the database
+        cur.execute(sqlite_insert_blob_query, data_tuple)    
+        connection.commit()
+        print("Image and file inserted successfully as a BLOB into a table")
+
+    return True
+
+def writeTofile(data, filename):
+    # Convert binary data to proper format and write it on Hard Disk
+    with open(filename, 'wb') as file:
+        file.write(data)
+        print("Stored blob data into ", filename, "\n")
+
+def readBlobData_by_id(connection: Connection, proj_id : int)->None:
+    with connection:
+        cur = connection.cursor()
+
+        sql_fetch_blob_query = """SELECT * 
+        FROM images WHERE project_id =?"""
+        cur.execute(sql_fetch_blob_query, (proj_id,))
+        image_list = []
+        rows = cur.fetchall()
+        for row in rows:
+            image_id = row[0]
+            image_title = row[1]
+            image_desc = row[2]
+            image_data = row[3]
+            image_type = row[4]
+            user_id = row[5]
+            project_id = row[6]
+            image = {"image_id": image_id,
+                     "image_title": image_title,
+                     "image_desc": image_desc,
+                     "image_data": image_data,
+                     "image_type": image_type,
+                     "project_id": project_id,
+                     "user_id": user_id}
+            image_list.append(ImageRetrieve.model_validate(image))
+        images = dict(images=image_list)   
+        return images
+    
 if __name__ == "__main__":
     connection = sqlite3.connect('harmony.db')
     connection.row_factory = sqlite3.Row
@@ -146,8 +221,8 @@ if __name__ == "__main__":
     #projects = get_user_projects(connection=connection, user_id=4)
     #for project in projects:
     #    print(dict(project))
-    project = get_project_by_id(connection=connection, proj_id=2)
-    print(project.model_dump())
+    #project = get_project_by_id(connection=connection, proj_id=2)
+    #print(project.model_dump())
     #user = get_user(connection, 'dibrown2')
     #print(user)
     #decor = Decor(decor_name='test decor', decor_type='wall', decor_desc='test decor description', user_id=1, project_id=1)
--- models.py
+++ models.py
@@ -14,19 +14,30 @@ class Project(UserProjectId):
 class Projects(BaseModel):
     projects: List[Project]
 
-class Decor(BaseModel):
-    decor_name: str
-    decor_type: str
-    decor_desc: str
-    user_id: int
+class Image(BaseModel):
+    image_title: str
+    image_desc: str
+    image_filename: str
+    image_type: str
     project_id: int
 
-class DecorImages(Decor):
-    decor_img_url: str
-    decor_blob: bytes
+class UserImage(Image):
+    user_id: int
 
-class DecorID(DecorImages):
-    decor_id: int
+class ImageId(UserImage):
+    image_id: int
+
+class ImageRetrieve(BaseModel):
+    image_id: int
+    image_title: str
+    image_desc: str
+    image_data: bytes
+    image_type: str
+    project_id: int
+    user_id: int
+
+class Images(BaseModel):
+    images: List[ImageRetrieve]
 
 class User(BaseModel):
     username: str
