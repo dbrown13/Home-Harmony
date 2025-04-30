8fdc72bf1697dccb3229ce994e0b8d93c35041c9
Deryn Brown <109038259+dbrown13@users.noreply.github.com>
2025-04-29 21:52:40 -0500

    Delete migrations/step2.create_room_table.sql

--- migrations/step2.create_room_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE rooms (
-    room_id INTEGER PRIMARY KEY,
-    room_name VARCHAR(50) NOT NULL,
-    room_desc VARCHAR(500),
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-);
