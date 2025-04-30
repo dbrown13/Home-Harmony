e89925629d07feffab616e3ac9392a63249fe3fe
Deryn Brown <109038259+dbrown13@users.noreply.github.com>
2025-04-29 21:52:54 -0500

    Delete Not Used directory

--- Not Used/contact.js
+++
@@ -1,7 +0,0 @@
-function contactForm() {
-  document.getElementById("myContactForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myContactForm").style.display = "none";
-}
